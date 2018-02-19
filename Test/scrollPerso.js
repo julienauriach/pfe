2d611ef1cd7660105147de3fb71018e05f66a4a7
Julien AURIACH <38929@PST6916.sciences-po.fr>
2018-02-19 17:43:43 +0100

    Changements de la journée
    JJJ

---
+++ Test/scrollPerso.js
@@ -0,0 +1,48 @@
+
+var controllerObjet1 = new ScrollMagic.Controller({
+    container: '#objet1',
+});
+
+var controller = new ScrollMagic.Controller();
+
+		// create a scene
+		
+	new ScrollMagic.Scene({
+		triggerElement: "#objet1",
+		triggerHook: 0,
+		duration: 547,
+        reverse: true
+    })
+	    .setPin("#objet1") // pins the element for the the scene's duration
+	    .addIndicators()
+	    .addTo(controller); // assign the scene to the controller
+
+	new ScrollMagic.Scene({
+		triggerElement: "#objet2",
+		triggerHook: 0,
+		duration: 547,
+        reverse: true
+    })
+    .setPin("#objet2") // pins the element for the the scene's duration
+    .addIndicators()
+    .addTo(controller); // assign the scene to the controller
+
+    new ScrollMagic.Scene({
+		triggerElement: "#objet3",
+		triggerHook: 0,
+		duration: 547,
+        reverse: true
+    })
+    .setPin("#objet3") // pins the element for the the scene's duration
+    .addIndicators()
+    .addTo(controller); // assign the scene to the controller
+
+    new ScrollMagic.Scene({
+		triggerElement: "#objet4",
+		triggerHook: 0,
+		duration: 547,
+        reverse: true
+    })
+    .setPin("#objet4") // pins the element for the the scene's duration
+    .addIndicators()
+    .addTo(controller); // assign the scene to the controller
