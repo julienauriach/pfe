69989fca9d5affb7b73446e7720aaf7e93bd77f4
Julien Auriach <31658208+julienauriach@users.noreply.github.com>
2018-03-12 18:28:20 +0100

    changements

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 
-var controllerObjet1 = new ScrollMagic.Controller({
-    container: '#objet1',
-});
+//         ARCHIVES
+
+// var controllerObjet1 = new ScrollMagic.Controller({
+//     container: '#objet1',
+// });
+
+    // new ScrollMagic.Scene({
+    //  triggerElement: "#objet1",
+    //  triggerHook: 0,
+    //  duration: 500,
+ //        reverse: true
+ //    })
+    //     .setPin("#objet1") // pins the element for the the scene's duration
+    //     .addIndicators()
+    //     .addTo(controller); // assign the scene to the controller
+    
 
 var controller = new ScrollMagic.Controller();
 
 		// create a scene
 		
-	new ScrollMagic.Scene({
-		triggerElement: "#objet1",
-		triggerHook: 0,
-		duration: 547,
-        reverse: true
-    })
-	    .setPin("#objet1") // pins the element for the the scene's duration
-	    .addIndicators()
-	    .addTo(controller); // assign the scene to the controller
+
 
 	new ScrollMagic.Scene({
 		triggerElement: "#objet2",
 		triggerHook: 0,
-		duration: 547,
+		duration: 500,
         reverse: true
     })
     .setPin("#objet2") // pins the element for the the scene's duration
@@ -30,7 +35,7 @@ var controller = new ScrollMagic.Controller();
     new ScrollMagic.Scene({
 		triggerElement: "#objet3",
 		triggerHook: 0,
-		duration: 547,
+		duration: 500,
         reverse: true
     })
     .setPin("#objet3") // pins the element for the the scene's duration
@@ -40,7 +45,7 @@ var controller = new ScrollMagic.Controller();
     new ScrollMagic.Scene({
 		triggerElement: "#objet4",
 		triggerHook: 0,
-		duration: 547,
+		duration: 500,
         reverse: true
     })
     .setPin("#objet4") // pins the element for the the scene's duration
