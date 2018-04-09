004d3600c7eb074c8f325d3e0623e2ff0905002b
Julien Auriach <31658208+julienauriach@users.noreply.github.com>
2018-04-09 18:48:08 +0200

    Le Changement Sacré de Jules

--- app.js
+++ app.js
@@ -1,55 +1,5 @@
 
-//         ARCHIVES
-
 ;(function() {
     // Initialize lazy loader
     var bLazy = new Blazy();
 })();
-// var controllerObjet1 = new ScrollMagic.Controller({
-//     container: '#objet1',
-// });
-
-    // new ScrollMagic.Scene({
-    //  triggerElement: "#objet1",
-    //  triggerHook: 0,
-    //  duration: 500,
- //        reverse: true
- //    })
-    //     .setPin("#objet1") // pins the element for the the scene's duration
-    //     .addIndicators()
-    //     .addTo(controller); // assign the scene to the controller
-    
-
-// var controller = new ScrollMagic.Controller();
-// 
-// 
-// 
-	// new ScrollMagic.Scene({
-		// triggerElement: "#objet2",
-		// triggerHook: 0,
-		// duration: 500,
-        // reverse: true
-    // })
-    // .setPin("#objet2") // pins the element for the the scene's duration
-    // .addIndicators()
-    // .addTo(controller); // assign the scene to the controller
-// 
-    // new ScrollMagic.Scene({
-		// triggerElement: "#objet3",
-		// triggerHook: 0,
-		// duration: 500,
-        // reverse: true
-    // })
-    // .setPin("#objet3") // pins the element for the the scene's duration
-    // .addIndicators()
-    // .addTo(controller); // assign the scene to the controller
-// 
-    // new ScrollMagic.Scene({
-		// triggerElement: "#objet4",
-		// triggerHook: 0,
-		// duration: 500,
-        // reverse: true
-    // })
-    // .setPin("#objet4") // pins the element for the the scene's duration
-    // .addIndicators()
-    // .addTo(controller); // assign the scene to the controller
--- cepages.vue
+++ cepages.vue
@@ -4,10 +4,31 @@
 		<p> Plusieurs cépages comme le <b>malbec</b> (France et Argentine), le <b>petit verdot</b> (France), le <b>sangiovese</b> (Italie), la <b>syrah</b> (France) ou encore le <b>touriga nacional</b> (Portugal), souvent plus résistants à la chaleur, pourraient remplacer le merlot. </p>
 		<p>À vous donc de créer un nouvel assemblage ! Choisissez un cépage et la proportion que vous souhaitez. Le reste de l’assemblage sera complété par du cabernet sauvignon. À quoi ressemblerait votre vin ?</p>
 		</div>
+    	<div class="glass row">
+    		<div class="col-4 text-center">
+    			<svg viewBox="0 0 337 518">
+					<path id="glass" d="M333.8,190.8C316.5,94.9,303.5,42.2,289.7,8.6L287.1,0H55.6l-3.5,8.6C38.3,42.3,26.2,94.1,4.6,190
+						c-12.1,51.8,0,107.1,32.8,152C63,377.5,99,401.9,137.9,411.4c0.8,3.3,1.6,6.7,2.2,10c1.7,9.6,3.4,22.6,5.1,38.2
+						c1.9,17.4,3.5,37.6,4.5,59.2h37.1c1-21.9,2.6-42.2,4.5-59.2c1.7-15.6,3.5-28.6,5.2-38.2c0.5-3,1.1-6,1.9-8.9
+						c34-7.4,67.6-28.6,93.1-60.2C328.7,307.4,344.2,248.2,333.8,190.8z M270.8,336.6c-28.6,35.5-69.2,55.4-105.6,52.8
+						c-40.6-3.5-79.6-26-107.3-63.2c-27.7-38.1-39-85.7-28.6-130.7C48.4,110.6,61.4,58.7,72.6,25.8h195.6c12.2,32,24.2,82.2,39.8,169.7
+						C316.7,245.7,303.7,297.6,270.8,336.6z"/>
+					<path id="wine" class="st0" d="M270.8,336.2c-28.6,35.5-69.2,55.4-105.6,52.8c-40.6-3.5-79.6-26-107.3-63.2
+						c-27.7-38.1-39-85.7-28.6-130.7C48.4,110.2,61.4,58.3,72.6,25.4h195.6c12.2,32,24.2,82.2,39.8,169.7
+						C316.7,245.3,303.7,297.2,270.8,336.2z" />
+					<mask id="wine_mask" x="0" y="0" width="337">
+					    <rect ref="maskRect" x="0" width="337" style="stroke:none; fill: #fff"/>
+					</mask>
+				</svg>
+    		</div>
+    		<div class="glass-message col-8 d-flex align-items-center" v-if="message !== null">
+    			<p>{{ message }}</p>
+    		</div>
+    	</div>
     	<ul class="list-unstyled row text-center">
     		<li class="col-12 col-sm-4" v-for="cepage in cepages">
-    			<button class="btn-primary raised" @click="selectCepage(cepage)">
-    				<img src="../../assets/images/grappe-01.svg" class="grappe"/> {{ cepage }}
+    			<button class="btn-primary raised btn-block" @click="selectCepage(cepage)">
+    				<img src="./assets/images/grappe-01.svg" class="grappe"/> {{ cepage }}
     			</button>
     			<div class="btn-group " v-if="selectedCepage === cepage">
     				<button @click="selectPercentage(percentage)" class="btn btn-outline-dark" v-for="percentage in percentages">
@@ -16,26 +37,6 @@
     			</div>
     		</li>
     	</ul>
-    	<div class="glass">
-    		<div class="glass-message" v-if="message !== null">
-    			{{ message }}
-    		</div>
-	    	<svg viewBox="0 0 337 518">
-				<path id="glass" d="M333.8,190.8C316.5,94.9,303.5,42.2,289.7,8.6L287.1,0H55.6l-3.5,8.6C38.3,42.3,26.2,94.1,4.6,190
-					c-12.1,51.8,0,107.1,32.8,152C63,377.5,99,401.9,137.9,411.4c0.8,3.3,1.6,6.7,2.2,10c1.7,9.6,3.4,22.6,5.1,38.2
-					c1.9,17.4,3.5,37.6,4.5,59.2h37.1c1-21.9,2.6-42.2,4.5-59.2c1.7-15.6,3.5-28.6,5.2-38.2c0.5-3,1.1-6,1.9-8.9
-					c34-7.4,67.6-28.6,93.1-60.2C328.7,307.4,344.2,248.2,333.8,190.8z M270.8,336.6c-28.6,35.5-69.2,55.4-105.6,52.8
-					c-40.6-3.5-79.6-26-107.3-63.2c-27.7-38.1-39-85.7-28.6-130.7C48.4,110.6,61.4,58.7,72.6,25.8h195.6c12.2,32,24.2,82.2,39.8,169.7
-					C316.7,245.7,303.7,297.6,270.8,336.6z"/>
-				<path id="wine" class="st0" d="M270.8,336.2c-28.6,35.5-69.2,55.4-105.6,52.8c-40.6-3.5-79.6-26-107.3-63.2
-					c-27.7-38.1-39-85.7-28.6-130.7C48.4,110.2,61.4,58.3,72.6,25.4h195.6c12.2,32,24.2,82.2,39.8,169.7
-					C316.7,245.3,303.7,297.2,270.8,336.2z" />
-				<mask id="wine_mask" x="0" y="0" width="337">
-				    <rect ref="maskRect" x="0" width="337" style="stroke:none; fill: #fff"/>
-				</mask>
-			</svg>
-    	</div>
-
     </div>
 </template>
 
@@ -116,7 +117,6 @@
 
 <style scoped>
 	.instructions {
-    max-width: 800px;
     position: relative;
     padding: 8px 30px;
 	}
@@ -143,19 +143,18 @@
 	margin-top: 3px;
 	}
 
-	.glass {
+/*	.glass {
 		border-top: 0;
 		position: relative;
-		max-width: 350px;
 		margin:10px auto;
 		display: block;
-	}
+	}*/
 
 	.glass-message {
-		position: absolute;
+		/*position: absolute;
 		top: 50%;
 		left: 50%;
-		transform: translate(-50%, -50%);
+		transform: translate(-50%, -50%);*/
 		font-size: 1.3em;
 		display: block;
 		padding: 0.3em;
@@ -167,7 +166,7 @@
 
 	.glass svg {
 		z-index: 0;
-		width:100%;
+		max-width: 150px;
 	}	
 
 	.glass svg #wine {
