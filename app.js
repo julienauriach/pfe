557d02804d723618e95b278c8be0dad46a181eea
Julien Auriach <julienauriach@macbook-air-de-julien.home>
2018-05-08 13:56:30 +0200

    Fin

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var controller = new ScrollMagic.Controller({addIndicators: true});
 
 // create a scene
 new ScrollMagic.Scene({
-		duration: 400,
-        triggerElement: "#hook0",
-        triggerHook: 0
+		duration: $("#hook0").height(),
+        triggerElement: "#hook0"
     })
+	.triggerHook(0)
     .setPin("#highcharts-3eb3ee9d-0bc5-4590-95d8-42a1aec79b11") 
     .addTo(controller)
