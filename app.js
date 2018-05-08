abd67e4176242038c9c43ba03263a207fc3f5f14
Julien Auriach <julienauriach@macbook-air-de-julien.home>
2018-05-08 12:39:42 +0200

    responsive adaptations

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // init controller
-var controller = new ScrollMagic.Controller();
+var controller = new ScrollMagic.Controller({addIndicators: true});
 
 // create a scene
 new ScrollMagic.Scene({
-        duration: 1000,
-        triggerElement: "#hook1"  // the scene should last for a scroll distance of 100px
-      // start this scene after scrolling for 50px
+		duration: 400,
+        triggerElement: "#hook0",
+        triggerHook: 0
     })
-    .setPin("#highcharts-3eb3ee9d-0bc5-4590-95d8-42a1aec79b11") // pins the element for the the scene's duration
-    .addTo(controller); // assign the scene to the controller
+    .setPin("#highcharts-3eb3ee9d-0bc5-4590-95d8-42a1aec79b11") 
+    .addTo(controller)
