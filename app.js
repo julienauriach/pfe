3e434333be4dd29f78c1bd13232dd2c5cb00053f
Julien Auriach <julienauriach@macbook-air-de-julien.home>
2018-05-08 10:59:41 +0200

    installation de ScrollMagic

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-$(function() {
-  $('#texteJQ').html('Hello world. Ce texte est affiché par jQuery.');
-});
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// create a scene
+new ScrollMagic.Scene({
+        duration: 1000,
+        triggerElement: "#hook1"  // the scene should last for a scroll distance of 100px
+      // start this scene after scrolling for 50px
+    })
+    .setPin("#highcharts-3eb3ee9d-0bc5-4590-95d8-42a1aec79b11") // pins the element for the the scene's duration
+    .addTo(controller); // assign the scene to the controller
