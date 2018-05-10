154b1883f8f8b448e96711334ab6863ad00607fb
Julien Auriach <julienauriach@MacBook-Air-de-Julien.local>
2018-05-10 09:54:39 +0200

    Changements importants

--- app.js
+++ app.js
@@ -9,3 +9,17 @@ new ScrollMagic.Scene({
 	.triggerHook(0)
     .setPin("#highcharts-3eb3ee9d-0bc5-4590-95d8-42a1aec79b11") 
     .addTo(controller)
+
+
+new ScrollMagic.Scene({
+		duration: $("#refPin1").height(),
+        triggerElement: "#refPin1"
+    })
+	.triggerHook(0)
+    .setPin("#vid") 
+    .addTo(controller)
+
+if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) 
+{
+   document.getElementsByTagName(".fondImgResponsive").className += " safari";
+}
