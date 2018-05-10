83b9ffaf8e47297a9a22f6b8d3f085393bc111c9
Julien Auriach <julienauriach@MacBook-Air-de-Julien.local>
2018-05-10 15:37:37 +0200

    changements de branche principale

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 // init controller
-var controller = new ScrollMagic.Controller({addIndicators: true});
 
 // create a scene
+
+new ScrollMagic.Scene({
+		duration: $("#refPin1").height(),
+        triggerElement: "#refPin1"
+    })
+	.triggerHook(0)
+    .setPin("#vid") 
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+	duration: $("#refPin2").height(),
+    triggerElement: "#refPin2"
+})
+.triggerHook(0)
+.setPin("#vid_1") 
+.addTo(controller);
+
 new ScrollMagic.Scene({
 		duration: $("#hook0").height(),
         triggerElement: "#hook0"
     })
-	.triggerHook(0)
-    .setPin("#highcharts-3eb3ee9d-0bc5-4590-95d8-42a1aec79b11") 
-    .addTo(controller)
+	.triggerHook(0.5)
+    .setPin("#highcharts-0b97d81e-bac1-44f8-857b-1e1f3c969c43") 
+    .addTo(controller);
