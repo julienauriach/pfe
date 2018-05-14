c6bf34022f3e04582b94663b56f3c86e00fc2e7f
julien <julien.auriach@gmail.com>
2018-05-14 23:35:28 +0200

    chgts

--- app.js
+++ app.js
@@ -3,26 +3,34 @@ var controller = new ScrollMagic.Controller();
 
 // create a scene
 
-new ScrollMagic.Scene({
-		duration: $("#refPin1").height(),
-        triggerElement: "#refPin1"
-    })
-	.triggerHook(0)
-    .setPin("#vid") 
-    .addTo(controller);
+// new ScrollMagic.Scene({
+// 		duration: $("#refPin1").height(),
+//         triggerElement: "#refPin1"
+//     })
+// 	.triggerHook(0)
+//     .setPin("#vid") 
+//     .addTo(controller);
 
-new ScrollMagic.Scene({
-	duration: $("#refPin2").height(),
-    triggerElement: "#refPin2"
-})
-.triggerHook(0)
-.setPin("#vid_1") 
-.addTo(controller);
+// new ScrollMagic.Scene({
+// 	duration: $("#refPin2").height(),
+//     triggerElement: "#refPin2"
+// })
+// .triggerHook(0)
+// .setPin("#vid_1") 
+// .addTo(controller);
 
-new ScrollMagic.Scene({
-		duration: $("#hook0").height(),
-        triggerElement: "#hook0"
-    })
-	.triggerHook(0.5)
-    .setPin("#highcharts-0b97d81e-bac1-44f8-857b-1e1f3c969c43") 
-    .addTo(controller);
+// new ScrollMagic.Scene({
+// 		duration: $("#hook0").height(),
+//         triggerElement: "#hook0"
+//     })
+// 	.triggerHook(0.5)
+//     .setPin("#highcharts-0b97d81e-bac1-44f8-857b-1e1f3c969c43") 
+//     .addTo(controller);
+
+// Highcharts
+
+$('a[data-toggle="tooltip"]').tooltip({
+    animated: 'fade',
+    placement: 'bottom',
+    trigger: 'click'
+});
